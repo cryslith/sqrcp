9aecdbee5d51e2fc26437366c2de93a88cf2aa7a
Lily Chung <lkdc@mit.edu>
2021-09-21 20:13:49 -0400

    easy size decreases

--- sqrcp-client/www/main.js
+++ sqrcp-client/www/main.js
@@ -1,79 +1,50 @@
 const S = window.sqrcp_client;
-console.log("top", S);
-
-async function decrypt(key, nonce, ciphertext) {
-console.log("dec", S);
-  return S.unseal(key, nonce, ciphertext);
-}
-
-async function readAll(stream, length) {
-  if (length < 64) {
-    length = 64;
-  }
-  let buffer = new Uint8Array(length);
-  let n = 0;
-  for (;;) {
-    let {done, value} = await stream.read();
-    if (done) {
-      break;
-    }
-    while (n + value.length > buffer.length) {
-      let buffer2 = new Uint8Array(buffer.length * 2);
-      buffer2.set(buffer, 0);
-      buffer = buffer2;
-    }
-    buffer.set(value, n);
-    n += value.length;
-  }
-  return buffer.slice(0, n);
-}
 
 async function main(params) {
-  console.log("main", S);
-  const { inline, ciphertext: cipherURL, key: keyBytes, nonce: nonceBytes, mimetype, filename, wasm, wasm_integrity } = params;
-
-  await S.init(fetch(wasm), {
-    integrity: wasm_integrity
-  });
-  S.set_panic_hook();
-
-  let message = document.getElementById("message");
+  const message = document.createElement("pre");
+  message.textContent = "Loading...";
+  document.body.appendChild(message);
 
   try {
-    let key = new Uint8Array(keyBytes);
-    let nonce = new Uint8Array(nonceBytes);
-    let response = await fetch(cipherURL);
+    const { inline, ciphertext: cipherURL, key: keyBytes, nonce: nonceBytes, mimetype, filename, wasm, wasm_integrity } = params;
+
+    await S.init(fetch(wasm), {
+      integrity: wasm_integrity
+    });
+    S.set_panic_hook();
+    const key = new Uint8Array(keyBytes);
+    const nonce = new Uint8Array(nonceBytes);
+    const response = await fetch(cipherURL);
     if (!response.ok) {
       throw new Error(`fetching ciphertext: ${response.statusText}`);
     }
-    // // response.arrayBuffer() is unfortunately not supported
-    // // on major mobile browsers yet
-    // let ciphertext = await readAll(response.body.getReader(),
-    //                                parseInt(response.headers.get("content-length"), 10) || -1);
-    let ciphertext = new Uint8Array(await response.arrayBuffer());
-    let plaintext = await decrypt(key, nonce, ciphertext);
+
+    const ciphertext = new Uint8Array(await response.arrayBuffer());
+    const plaintext = S.unseal(key, nonce, ciphertext);
 
     if (inline) {
-      let dataview = new DataView(plaintext);
-      let shorts = [];
+      const dataview = new DataView(plaintext);
+      const shorts = [];
       for (let i = 0; i < dataview.byteLength; i += 2) {
         shorts.push(dataview.getUint16(i, true));
       }
-      let text = String.fromCharCode.apply(null, shorts);
-      document.getElementById("plaintext").textContent = text;
+      const text = String.fromCharCode.apply(null, shorts);
+      const plaintext = document.createElement("pre");
+      plaintext.textContent = text;
+      document.body.appendChild(plaintext);
     } else {
-      let download = document.getElementById("download");
-      let blob = new Blob([plaintext], {type: mimetype});
+      const download = document.createElement("a");
+      const blob = new Blob([plaintext], {type: mimetype});
       download.setAttribute("href", URL.createObjectURL(blob));
       download.setAttribute("download", filename);
       download.textContent = "Open file";
+      document.body.appendChild(download);
     }
   } catch (e) {
     message.textContent = `error: ${e}`;
     throw e;
   }
 
-  console.log("success");
   message.parentNode.removeChild(message);
 }
 
